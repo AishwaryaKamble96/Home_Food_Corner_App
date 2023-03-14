94fd79dab6f84130207310abeb370781852276f6
AishwaryaKamble96 <aishwaryakamblegem@gmail.com>
2023-03-14 14:43:40 +0100

    completed user story1

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,8 @@
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import TitleBar from "../component/TitleBar";
-import Footer from "../component/Navigation";
-import Post from "../component/Post";
-import styled from "styled-components";
+
 //const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <Wrapper imgUrl={process.env.PUBLIC_URL + "/background_img.jpg"}>
-        Hi
-      </Wrapper>
-    </>
-  );
+  return <></>;
 }
-
-const Wrapper = styled.div``;
