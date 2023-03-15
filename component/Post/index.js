10468ec963fce924c55a5dbdffaa8c7ae7a87b27
AishwaryaKamble96 <aishwaryakamblegem@gmail.com>
2023-03-15 18:03:53 +0100

    adding search conitioning

--- component/Post/index.js
+++ component/Post/index.js
@@ -1,9 +1,45 @@
+import styled from "styled-components";
+import Image from "next/image";
+
 export default function Post({ postData }) {
   return (
     <>
-      <li key={postData._id}>
-        <h3>{postData.name}</h3>
-      </li>
+      <PostWrapper key={postData._id}>
+        <ImageWrapper>
+          <Image
+            src={postData.image_url}
+            alt={postData.name}
+            width={170}
+            height={200}
+            priority
+          ></Image>
+        </ImageWrapper>
+        <InfoWrapper>
+          <h3>{postData.name}</h3>
+          <dd>Price:€ {postData.price} </dd>
+          <dd>Tag: {postData.tag} </dd>
+          <dd>Available on :{postData.date_of_availability}</dd>
+          <dd>Location: {postData.location} </dd>
+        </InfoWrapper>
+      </PostWrapper>
     </>
   );
 }
+
+const ImageWrapper = styled.div`
+  border-radius: 50%;
+`;
+const PostWrapper = styled.li`
+  margin: 0 10px 0 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  /* border-radius: 7px; */
+  padding: 20px;
+  background-color: rgb(251, 252, 252);
+  /* box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px; */
+  position: relative;
+  width: 100%;
+`;
+
+const InfoWrapper = styled.div``;
--- pages/index.js
+++ pages/index.js
@@ -18,20 +18,81 @@ export default function Home() {
     fetchData().catch(console.error);
   }, []);
 
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearch(event) {
+    event.preventDefault();
+    if (postList.length <= 1) {
+      console.log("ëmty");
+      setPostList(postList);
+    }
+    ///setPostList(postList);
+    setSearchText(event.target.value);
+
+    console.log("called", searchText);
+    // const searchResult = postList.filter((post) => {
+    //   const postName = post.name.toLowerCase();
+    //   return postName.includes(searchText);
+    // });
+    const searchResult = postList.map((post) => {
+      const postName = post.name.toLowerCase();
+      return postName.includes(searchText);
+    });
+
+    setPostList(searchResult);
+    console.log(searchResult);
+    // if (!searchResult) {
+    //   return <h2>Sorry!! Search for something else.</h2>;
+    // }
+  }
+
+  async function handleReload(event) {
+    // const form = event.target;
+    // form.reset();
+    const data = await fetch("/api/posts");
+    const postData = await data.json();
+    setPostList(postData);
+  }
+
   return (
     <>
-      <section>
+      <AppGrid>
+        {/* ///onSubmit={handleSearch} */}
+        <form>
+          <SearchBar
+            typeof="search"
+            id="searchText"
+            name="searchText"
+            placeholder="Enter your favourite food name"
+            onChange={handleSearch}
+          ></SearchBar>
+
+          <button onClick={handleReload}>Refresh</button>
+        </form>
         <PostList>
           {postList.map((post) => {
             return <Post postData={post}></Post>;
           })}
         </PostList>
-      </section>
+      </AppGrid>
     </>
   );
 }
 
+const AppGrid = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
 const PostList = styled.ul`
   display: flex;
   flex-direction: column;
+  gap: 20px;
+  align-items: center;
+`;
+
+const SearchBar = styled.input`
+  width: 80%;
+  border-radius: 1%;
+  align-self: center;
+  background-color: rgb(253, 254, 254);
 `;
